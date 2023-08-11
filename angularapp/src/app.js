fca16be3ae46e85dba3a6e004f732be20c731fba
demo-college <d15784889@gmail.com>
2023-08-11 17:38:45 +0000

    apps.js

---
+++ angularapp/src/app.js
@@ -0,0 +1,429 @@
+
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+console.log("apps js")
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("ks")
+}
